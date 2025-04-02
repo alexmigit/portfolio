fa2c1cc1a68b05d56668bec4a3f7e7c01f74c3e9
Alex Migit <migit001@umn.edu>
2025-04-02 16:54:57 -0500

    Added staging model

---
+++ projects/dbt_healthcare_project/business_intelligence/models/staging/stg_patients.sql
@@ -0,0 +1,30 @@
+
+WITH source AS (
+    SELECT
+        patient_id,
+        first_name,
+        last_name,
+        date_of_birth,
+        gender,
+        last_visit_date,
+        insurance_provider,
+        diagnosis_code,
+        created_at,
+        updated_at
+    FROM {{ source('EHR', 'patients') }}
+)
+SELECT
+    patient_id,
+    UPPER(first_name) AS first_name,
+    UPPER(last_name) AS last_name,
+    CAST(date_of_birth AS DATE) AS dob,
+    gender,
+    diagnosis_code,
+    insurance_provider,
+    last_visit_date,
+    created_at,
+    updated_at
+FROM
+    source
+WHERE
+    patient_id IS NOT NULL;
